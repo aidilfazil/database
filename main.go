8a277d22436382ffbd2db92b296f45c5746608df
aidilfazil <aidilfazil02@gmail.com>
2024-07-02 21:44:12 +0800

    ubah

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"go.mongodb.org/mongo-driver/bson"
-	
 )
 
 type Car struct {
@@ -27,21 +26,20 @@ type Car struct {
 }
 
 type Customer struct {
-	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name          string             `json:"name"`
-	Email         string             `json:"email"`
-	PhoneNumber   string             `json:"phone_number"`
-	DriversLicense string            `json:"drivers_license,omitempty"`
+	ID             primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name           string             `json:"name"`
+	Email          string             `json:"email"`
+	PhoneNumber    string             `json:"phone_number"`
+	DriversLicense string             `json:"drivers_license,omitempty"`
 }
 
 type Rental struct {
-	ID               primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	CarID            primitive.ObjectID `json:"car_id"`
-	CustomerID       primitive.ObjectID `json:"customer_id"`
-	RentalStartDate  string             `json:"rental_start_date"`
-	RentalEndDate    string             `json:"rental_end_date"`
+	ID              primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	CarID           primitive.ObjectID `json:"car_id"`
+	CustomerID      primitive.ObjectID `json:"customer_id"`
+	RentalStartDate string             `json:"rental_start_date"`
+	RentalEndDate   string             `json:"rental_end_date"`
 }
-
 
 var carCollection *mongo.Collection
 var customerCollection *mongo.Collection
@@ -84,7 +82,6 @@ func main() {
 	app.Patch("/api/cars/:id", updateCar)
 	app.Delete("/api/cars/:id", deleteCar)
 
-	
 	app.Get("/api/customer/:id", getCustomerByID)
 	app.Get("/api/customers", getCustomers)
 	app.Post("/api/customers", createCustomer)
@@ -98,7 +95,7 @@ func main() {
 	app.Delete("/api/rentals/:id", deleteRental)
 
 	port := os.Getenv("PORT")
-	if port == ""{
+	if port == "" {
 		port = "5000"
 	}
 
@@ -110,24 +107,23 @@ func main() {
 }
 
 func getCarByID(c *fiber.Ctx) error {
-    id := c.Params("id")
-    objectID, err := primitive.ObjectIDFromHex(id)
-    if err != nil {
-        return c.Status(400).JSON(fiber.Map{"error": "Invalid car ID"})
-    }
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid car ID"})
+	}
 
-    var car Car
-    err = carCollection.FindOne(context.Background(), bson.M{"_id": objectID}).Decode(&car)
-    if err != nil {
-        if err == mongo.ErrNoDocuments {
-            return c.Status(404).JSON(fiber.Map{"error": "Car not found"})
-        }
-        return err
-    }
+	var car Car
+	err = carCollection.FindOne(context.Background(), bson.M{"_id": objectID}).Decode(&car)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return c.Status(404).JSON(fiber.Map{"error": "Car not found"})
+		}
+		return err
+	}
 
-    return c.JSON(car)
+	return c.JSON(car)
 }
-
 
 func getCars(c *fiber.Ctx) error {
 	var cars []Car
@@ -158,53 +154,50 @@ func createCar(c *fiber.Ctx) error {
 }
 
 func updateCar(c *fiber.Ctx) error {
-    id := c.Params("id")
-    objectID, err := primitive.ObjectIDFromHex(id)
-    if err != nil {
-        return c.Status(400).JSON(fiber.Map{"error": "Invalid car ID"})
-    }
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid car ID"})
+	}
 
-    var car Car
-    if err := c.BodyParser(&car); err != nil {
-        return err
-    }
+	var car Car
+	if err := c.BodyParser(&car); err != nil {
+		return err
+	}
 
-    update := bson.M{}
-    if car.Make != "" {
-        update["make"] = car.Make
-    }
-    if car.Model != "" {
-        update["model"] = car.Model
-    }
-    if car.Year != 0 {
-        update["year"] = car.Year
-    }
-    if car.Type != "" {
-        update["type"] = car.Type
-    }
-    // Check presence of the 'available' field in the request body
-    var body map[string]interface{}
-    if err := c.BodyParser(&body); err != nil {
-        return err
-    }
-    if available, ok := body["available"]; ok {
-        update["available"] = available
-    }
+	update := bson.M{}
+	if car.Make != "" {
+		update["make"] = car.Make
+	}
+	if car.Model != "" {
+		update["model"] = car.Model
+	}
+	if car.Year != 0 {
+		update["year"] = car.Year
+	}
+	if car.Type != "" {
+		update["type"] = car.Type
+	}
+	// Check presence of the 'available' field in the request body
+	var body map[string]interface{}
+	if err := c.BodyParser(&body); err != nil {
+		return err
+	}
+	if available, ok := body["available"]; ok {
+		update["available"] = available
+	}
 
-    if len(update) == 0 {
-        return c.Status(400).JSON(fiber.Map{"error": "No valid fields to update"})
-    }
+	if len(update) == 0 {
+		return c.Status(400).JSON(fiber.Map{"error": "No valid fields to update"})
+	}
 
-    updateSet := bson.M{"$set": update}
-    _, err = carCollection.UpdateOne(context.Background(), bson.M{"_id": objectID}, updateSet)
-    if err != nil {
-        return err
-    }
-    return c.Status(200).JSON(fiber.Map{"success": true})
+	updateSet := bson.M{"$set": update}
+	_, err = carCollection.UpdateOne(context.Background(), bson.M{"_id": objectID}, updateSet)
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
-
-
-
 
 func deleteCar(c *fiber.Ctx) error {
 	id := c.Params("id")
@@ -220,24 +213,23 @@ func deleteCar(c *fiber.Ctx) error {
 }
 
 func getCustomerByID(c *fiber.Ctx) error {
-    id := c.Params("id")
-    objectID, err := primitive.ObjectIDFromHex(id)
-    if err != nil {
-        return c.Status(400).JSON(fiber.Map{"error": "Invalid customer ID"})
-    }
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid customer ID"})
+	}
 
-    var customer Customer
-    err = customerCollection.FindOne(context.Background(), bson.M{"_id": objectID}).Decode(&customer)
-    if err != nil {
-        if err == mongo.ErrNoDocuments {
-            return c.Status(404).JSON(fiber.Map{"error": "Customer not found"})
-        }
-        return err
-    }
+	var customer Customer
+	err = customerCollection.FindOne(context.Background(), bson.M{"_id": objectID}).Decode(&customer)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return c.Status(404).JSON(fiber.Map{"error": "Customer not found"})
+		}
+		return err
+	}
 
-    return c.JSON(customer)
+	return c.JSON(customer)
 }
-
 
 func getCustomers(c *fiber.Ctx) error {
 	var customers []Customer
@@ -268,44 +260,42 @@ func createCustomer(c *fiber.Ctx) error {
 }
 
 func updateCustomer(c *fiber.Ctx) error {
-    id := c.Params("id")
-    objectID, err := primitive.ObjectIDFromHex(id)
-    if err != nil {
-        return c.Status(400).JSON(fiber.Map{"error": "Invalid customer ID"})
-    }
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid customer ID"})
+	}
 
-    var customer Customer
-    if err := c.BodyParser(&customer); err != nil {
-        return err
-    }
+	var customer Customer
+	if err := c.BodyParser(&customer); err != nil {
+		return err
+	}
 
-    update := bson.M{}
-    if customer.Name != "" {
-        update["name"] = customer.Name
-    }
-    if customer.Email != "" {
-        update["email"] = customer.Email
-    }
-    if customer.PhoneNumber != "" {
-        update["phone_number"] = customer.PhoneNumber
-    }
-    if customer.DriversLicense != "" {
-        update["drivers_license"] = customer.DriversLicense
-    }
+	update := bson.M{}
+	if customer.Name != "" {
+		update["name"] = customer.Name
+	}
+	if customer.Email != "" {
+		update["email"] = customer.Email
+	}
+	if customer.PhoneNumber != "" {
+		update["phone_number"] = customer.PhoneNumber
+	}
+	if customer.DriversLicense != "" {
+		update["drivers_license"] = customer.DriversLicense
+	}
 
-    if len(update) == 0 {
-        return c.Status(400).JSON(fiber.Map{"error": "No valid fields to update"})
-    }
+	if len(update) == 0 {
+		return c.Status(400).JSON(fiber.Map{"error": "No valid fields to update"})
+	}
 
-    updateSet := bson.M{"$set": update}
-    _, err = customerCollection.UpdateOne(context.Background(), bson.M{"_id": objectID}, updateSet)
-    if err != nil {
-        return err
-    }
-    return c.Status(200).JSON(fiber.Map{"success": true})
+	updateSet := bson.M{"$set": update}
+	_, err = customerCollection.UpdateOne(context.Background(), bson.M{"_id": objectID}, updateSet)
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
-
-
 
 func deleteCustomer(c *fiber.Ctx) error {
 	id := c.Params("id")
@@ -321,24 +311,23 @@ func deleteCustomer(c *fiber.Ctx) error {
 }
 
 func getRentalByID(c *fiber.Ctx) error {
-    id := c.Params("id")
-    objectID, err := primitive.ObjectIDFromHex(id)
-    if err != nil {
-        return c.Status(400).JSON(fiber.Map{"error": "Invalid rental ID"})
-    }
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid rental ID"})
+	}
 
-    var rental Rental
-    err = rentalCollection.FindOne(context.Background(), bson.M{"_id": objectID}).Decode(&rental)
-    if err != nil {
-        if err == mongo.ErrNoDocuments {
-            return c.Status(404).JSON(fiber.Map{"error": "Rental not found"})
-        }
-        return err
-    }
+	var rental Rental
+	err = rentalCollection.FindOne(context.Background(), bson.M{"_id": objectID}).Decode(&rental)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return c.Status(404).JSON(fiber.Map{"error": "Rental not found"})
+		}
+		return err
+	}
 
-    return c.JSON(rental)
+	return c.JSON(rental)
 }
-
 
 func getRentals(c *fiber.Ctx) error {
 	var rentals []Rental
@@ -369,42 +358,42 @@ func createRental(c *fiber.Ctx) error {
 }
 
 func updateRental(c *fiber.Ctx) error {
-    id := c.Params("id")
-    objectID, err := primitive.ObjectIDFromHex(id)
-    if err != nil {
-        return c.Status(400).JSON(fiber.Map{"error": "Invalid rental ID"})
-    }
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid rental ID"})
+	}
 
-    var rental Rental
-    if err := c.BodyParser(&rental); err != nil {
-        return err
-    }
+	var rental Rental
+	if err := c.BodyParser(&rental); err != nil {
+		return err
+	}
 
-    update := bson.M{}
-    if !rental.CarID.IsZero() {
+	update := bson.M{}
+	if !rental.CarID.IsZero() {
 		update["car_id"] = rental.CarID
 	}
 	if !rental.CustomerID.IsZero() {
 		update["customer_id"] = rental.CustomerID
-	}	
-    if rental.RentalStartDate != "" {
+	}
+	if rental.RentalStartDate != "" {
 		update["rental_start_date"] = rental.RentalStartDate
 	}
-	
-    if 	rental.RentalEndDate != "" {
-        update["rental_end_date"] = rental.RentalEndDate
-    }
 
-    if len(update) == 0 {
-        return c.Status(400).JSON(fiber.Map{"error": "No valid fields to update"})
-    }
+	if rental.RentalEndDate != "" {
+		update["rental_end_date"] = rental.RentalEndDate
+	}
 
-    updateSet := bson.M{"$set": update}
-    _, err = rentalCollection.UpdateOne(context.Background(), bson.M{"_id": objectID}, updateSet)
-    if err != nil {
-        return err
-    }
-    return c.Status(200).JSON(fiber.Map{"success": true})
+	if len(update) == 0 {
+		return c.Status(400).JSON(fiber.Map{"error": "No valid fields to update"})
+	}
+
+	updateSet := bson.M{"$set": update}
+	_, err = rentalCollection.UpdateOne(context.Background(), bson.M{"_id": objectID}, updateSet)
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
 
 func deleteRental(c *fiber.Ctx) error {
